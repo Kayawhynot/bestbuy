45051d17421927a3cb688dbd82b839235d9ee345
Kaya <kayawhynot@gmail.com>
2026-08-31 18:21:02 +0200

    create Product class

---
+++ products.py
@@ -0,0 +1,37 @@
+class Product:
+
+    def __init__(self,name, price, quantity):
+        if name == "" or price < 0 or quantity < 0:
+            raise Exception("Name can't be empty. Price and quantity must be a positive!")
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+    def get_quantity(self):
+        return self.quantity
+
+    def set_quantity(self, quantity)
+        self.quantity = quantity
+        if self.quantity == 0:
+            self.active = False
+            print("Your ordered quantity is not available - more is on its' way")
+
+    def is_active(self):
+        return self.active
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self):
+        print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
+
+    def buy(self, quantity):
+        if quantity < 0 or quantity > self.quantity:
+            raise Exception(f"The quantity {quantity} is not available. The max. you can order is: {self.quantity}")
+        total_price = quantity * self.price
+        self.set_quantity(self.quantity - quantity)
+        return total_price
